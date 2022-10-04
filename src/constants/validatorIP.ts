b1a97872b58eee4f476992e8ecd36247bf9e6c55
TheWarrior09 <the.warrior.0072@gmail.com>
2022-10-04 18:06:45 +0500

    Create separate file for constant validatorIP

---
+++ src/constants/validatorIP.ts
@@ -0,0 +1,8 @@
+export const validatorIPAddresses = [
+  { ip: '164.92.191.219', name: 'Frankfurt' },
+  { ip: '206.189.33.200', name: 'Singapore' },
+  { ip: '134.122.38.54', name: 'Toronto' },
+  { ip: '164.92.157.54', name: 'Amsterdam' },
+  { ip: '188.166.145.51', name: 'London' },
+  { ip: '165.232.134.41', name: 'Santa Clara' },
+];
